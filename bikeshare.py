c2efcbedf7754ac19fb1c7b816271527cf2007b8
scotthedad <ssyoon@yahoo.com>
2023-12-21 20:41:00 -0500

    Refactoring Branch Update - README.md and bikeshare.py comments updated

--- bikeshare.py
+++ bikeshare.py
@@ -1,4 +1,5 @@
 # I just realized that i had to close and open new Terminals to refresh and see the correct output on the terminal, eg, even after code update or add, terminal won't show the output from the update or added codes. Not knowing this, I spent much time trying to debut my codes unecessarily.  Perhaps a disclaimer is warranted.
+# this is for refactoring branch update
 
 import time
 import pandas as pd
